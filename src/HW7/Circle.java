d9496971bad614360634c06c2307fae0b8a61961
max <maksym.borovskyi@smartyads.com>
2024-04-02 19:08:54 +0300

    test commint...

---
+++ src/HW7/Circle.java
@@ -0,0 +1,10 @@
+package HW7;
+
+public class Circle extends Figures{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+}
---
+++ src/HW7/Figures.java
@@ -0,0 +1,17 @@
+package HW7;
+
+class Figures {
+    // Пустой конструктор
+    public Figures() {
+    }
+
+    // Метод для получения площади фигуры
+    public double getArea() {
+        return 0.0; // Возвращаем 0 для абстрактной фигуры
+    }
+
+    // Метод для получения периметра фигуры
+    public double getPerimeter() {
+        return 0.0; // Возвращаем 0 для абстрактной фигуры
+    }
+}
